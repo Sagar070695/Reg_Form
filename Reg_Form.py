b6fb179e4228fa112557669d78575059b77a0115
Sagar070695 <161796603+Sagar070695@users.noreply.github.com>
2024-12-11 22:15:41 +0530

    Add files via upload

---
+++ Reg_Form.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+# Set the page title
+st.title("User Registration Form")
+# Add a text input with a caption
+name = st.text_input("Enter your name")
+email = st.text_input("Enter your email")
+pwd = st.text_input("Enter your password", type="password")
+
+# Add a button with the caption "Register"
+st.button("Register")
